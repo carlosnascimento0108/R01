b996a824cb48b81673e609601a455464b3758a3e
Carlos Leal <carlsnascimento72@gmail.com>
2025-11-18 15:13:49 -0300

    Update BubbleSort.java

--- src/main/java/sorting/simpleSorting/BubbleSort.java
+++ src/main/java/sorting/simpleSorting/BubbleSort.java
@@ -12,12 +12,15 @@ public class BubbleSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		
+		boolean swapped = true;
+
 		if (leftIndex >=0 && rightIndex >= 0 && leftIndex < rightIndex) {
-			for (int i =leftIndex; i < rightIndex; i++) { 
-				for (int j = leftIndex; j < rightIndex - 1; j++) {
-					if (array[j].compareTo(array[j+1]) > 0) {
-						Util.swap(array, j, j+1);
+			while (swapped) {
+				swapped = false; 
+				for (int i = leftIndex; i < rightIndex - i; i++) {
+					if (array[i].compareTo(array[i+1]) > 0) {
+						Util.swap(array, i, i+1);
+						swapped = true;
 					}
 				}
 			}
