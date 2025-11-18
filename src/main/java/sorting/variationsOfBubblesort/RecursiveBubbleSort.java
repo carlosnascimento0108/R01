3796e91e66b98be9400809083b128f8258f96a00
Carlos Leal <carlsnascimento72@gmail.com>
2025-11-18 15:15:24 -0300

    Update RecursiveBubbleSort.java

--- src/main/java/sorting/variationsOfBubblesort/RecursiveBubbleSort.java
+++ src/main/java/sorting/variationsOfBubblesort/RecursiveBubbleSort.java
@@ -15,8 +15,23 @@ public class RecursiveBubbleSort<T extends Comparable<T>> extends
 	 */
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if (leftIndex >= rightIndex) {
+			return;
+		}
+
+		for (int i = leftIndex; i < rightIndex; i++) {
+				if (array[i].compareTo(array[i+1]) > 0) {
+					T swap = array[i];
+					array[i] = array[i+1];
+					array[i+1] = swap;
+
+				}
+			}
+		
+			if (leftIndex >=0 && rightIndex >= 0 && leftIndex < rightIndex) {
+				sort(array, leftIndex, rightIndex - 1);
+			
+		}
 	}
 
 }
